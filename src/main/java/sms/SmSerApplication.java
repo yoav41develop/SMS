3c4c783952ea3ec5b86bfc2d0e0648e90f899e24
Meir Malka <nthr1984@hotmail.com>
2026-02-09 16:17:36 +0200

    First

---
+++ src/main/java/sms/SmSerApplication.java
@@ -0,0 +1,13 @@
+package sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmSerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmSerApplication.class, args);
+	}
+
+}
