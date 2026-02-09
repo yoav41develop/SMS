9a222fcbfaa7f98cc52da742ba88464798ac4e58
Meir Malka <nthr1984@hotmail.com>
2026-02-09 17:32:44 +0200

    Fix added @RestController

--- src/main/java/sms/SMSManager.java
+++ src/main/java/sms/SMSManager.java
@@ -4,7 +4,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.annotation.Resource;
+
+@RestController
 public class SMSManager {
 	@GetMapping(value = "/subscribe/{phoneNumber}")
 		public ResponseEntity<String> subscribe(@PathVariable String phoneNumber)  {
