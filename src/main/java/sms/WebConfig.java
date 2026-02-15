9b93936b5ed3b6cd8fa7086d99f881008a13808c
Meir Malka <nthr1984@hotmail.com>
2026-02-15 14:00:10 +0200

    Added CORS conf

---
+++ src/main/java/sms/WebConfig.java
@@ -0,0 +1,22 @@
+package sms;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://fir-autocallevents.web.app")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
